b36415d255c97d57d5c4c2a33e6bcad60ac2aaf6
Beshliu <vozidla@bk.ru>
2025-07-25 19:39:40 +0700

    Выполнил второе домашнее

---
+++ Lang/lan.js
@@ -0,0 +1,16 @@
+const lan = prompt('Какой язык использовать?')
+
+switch (lan) {
+    case 'RU':
+       console.log('Добрый день!');
+    break;
+    case 'EN':
+       console.log('Good afternoon!');
+    break;
+    case 'DE':
+       console.log('Guten Tag!');
+    break;
+    case 'FR':
+       console.log('Bonjour!');
+    break;
+}
